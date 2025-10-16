d3fef14486c2933a4b3cb6538d7d3d718069a770
Chris <104177004+Chris-THC@users.noreply.github.com>
2025-10-15 19:31:19 -0600

    fix: f_cuenta_registros_por_institucion_all() (#19)

---
+++ db/functions/f_cuenta_registros_por_institucion_all.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE FUNCTION f_cuenta_registros_por_institucion_all()
+RETURNS TABLE (
+    id_institucion BIGINT,
+    institucion_nombre TEXT,
+    tipo_institucion TEXT,
+    total BIGINT
+)
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        ins.id_institucion,
+        ins.nombre::TEXT AS institucion_nombre,
+        p.nombre::TEXT AS tipo_institucion,
+        COUNT(DISTINCT r.id_registro)::BIGINT AS total
+    FROM institucion ins
+    INNER JOIN parametrizacion p
+        ON ins.tipo_institucion_param = p.id_param
+    LEFT JOIN adscripcion a
+        ON ins.id_institucion = a.id_institucion
+    LEFT JOIN registro_investigador ri
+        ON a.id_investigador = ri.id_investigador
+    LEFT JOIN registro r
+        ON r.id_registro = ri.id_registro
+    GROUP BY ins.id_institucion, ins.nombre, p.nombre
+    ORDER BY total DESC;
+END;
+$$ LANGUAGE plpgsql;
